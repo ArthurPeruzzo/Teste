f11523a0d23e0453df3c2e0a2925049e3f8b2adc
ArthurPeruzzo <arthurkohl0@gmail.com>
2021-06-11 15:25:16 -0300

    criacao das operacoes basicas da classe Avaliacao

---
+++ src/main/java/br/inf/ids/educacao/resources/AvaliacaoResource.java
@@ -0,0 +1,48 @@
+package br.inf.ids.educacao.resources;
+
+import br.inf.ids.educacao.excecoes.Exceptions;
+import br.inf.ids.educacao.models.Avaliacao;
+import br.inf.ids.educacao.models.TipoAvaliacao;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+@RequestScoped
+public class AvaliacaoResource {
+
+    @Inject
+    EntityManager entityManager;
+
+    public Avaliacao cadastrarAvaliacao(Avaliacao avaliacao){
+        entityManager.persist(avaliacao);
+        entityManager.flush();
+        return avaliacao;
+    }
+
+    public Avaliacao buscarAvaliacao(Long id) {
+        Avaliacao avaliacao;
+        try{
+        avaliacao= entityManager.find(Avaliacao.class, id);
+        }catch (RuntimeException e) {
+            throw new Exceptions(id);
+        }
+        return avaliacao;
+    }
+
+    public void updateAvaliacao(Avaliacao avaliacao) {
+        entityManager.merge(avaliacao);
+        entityManager.flush();
+    }
+
+    public void deleteAvaliacao(Long id) {
+        try{
+           Avaliacao avaliacao = buscarAvaliacao(id);
+            entityManager.remove(avaliacao);
+            entityManager.flush();
+        }catch (IllegalArgumentException e){
+            throw new Exceptions(id);
+        }
+
+    }
+}
---
+++ src/main/java/br/inf/ids/educacao/rest/AvaliacaoRest.java
@@ -0,0 +1,47 @@
+package br.inf.ids.educacao.rest;
+
+import br.inf.ids.educacao.models.Avaliacao;
+import br.inf.ids.educacao.resources.AvaliacaoResource;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/Avaliacao")
+public class AvaliacaoRest {
+
+    @Inject
+    AvaliacaoResource avaliacaoResource;
+
+    @POST
+    @Path("/cadastroAvaliacao")
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    public Avaliacao cadastrarAvaliacao(Avaliacao avaliacao){
+        return avaliacaoResource.cadastrarAvaliacao(avaliacao);
+    }
+
+    @GET
+    @Path("/{id}")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Avaliacao buscarAvaliacao(@PathParam("id") Long id){
+        return avaliacaoResource.buscarAvaliacao(id);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void deletarTipoAvaliacao(@PathParam("id") Long id){
+        avaliacaoResource.deleteAvaliacao(id);
+    }
+
+    @PUT
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void uptadeTipoAvaliacao(Avaliacao avaliacao){
+        avaliacaoResource.updateAvaliacao(avaliacao);
+    }
+}
